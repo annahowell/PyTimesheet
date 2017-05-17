c34a87b5b773ba9f925705c1853cc56e14c76431
annabel82 <abjt82@gmail.com>
2017-05-17 17:16:51 +0100

    templatetags

--- employee/forms.py
+++ employee/forms.py
@@ -2,7 +2,22 @@ from django import forms
 
 from .models import Employee
 
+
 class PostForm(forms.ModelForm):
+
+    first_name = forms.CharField(label="First name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'first_name'}))
+
+
+    username = forms.CharField(
+        label="Username",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'name': 'username'
+                   }
+        )
+    )
+    password = forms.CharField(label="Password", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
 
     class Meta:
         model = Employee
